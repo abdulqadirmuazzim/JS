a8396997e9c37e6067bb3c1a8e6e4ef65f88c074
abdul <abdulqadimuazzim@gmail.com>
2023-06-12 22:57:12 +0100

    Tracking progress on c++ & javascript lang

--- ap.cpp
+++ ap.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
-
-// type variablename = valie;
-int num = 66;
-double getit = 12.434343;
-char itit = 'A';
-string str = "this is a test";
-
-bool ff = true;
-
-double sum = num + getit;
 
 int main()
 {
-    double x, y, z;
-    x = 3e-8;
-    y = 2e9;
-    z = 4.55e+2;
-    cout << (y <= z && x <= z);
-    return 0;
+    int name = 8;
+    for (int a = 0; a < 5; a++)
+    {
+        switch (name)
+        {
+        case 1:
+            cout << "ssss\n";
+        case 2:
+            cout << "key";
+        default:
+            cout << "I get it now, using single quotes returns an error\n";
+        }
+    }
 }
---
+++ this.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "your name:";
+    cin >> name;
+    cout << name + ", how are you doing" << endl;
+}
